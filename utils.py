dc78a4d94891fae9af24f6ccd219299d4c67e461
mlapusta <lapusta.mariia@gmail.com>
2026-02-17 20:59:08 +0100

    feature: get filenames from path

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from contextlib import chdir
 
 def create_folder(path: str, name: str) -> None:
     folder_path = os.path.join(path, name)
@@ -6,3 +7,20 @@ def create_folder(path: str, name: str) -> None:
         os.mkdir(folder_path)
     except FileExistsError as e:
         print(e)
+
+def get_all_filenames(path: str) -> list:
+    try:
+        with chdir(path):
+            file_list = os.listdir(path)
+            return file_list
+    except FileNotFoundError as e:
+        print(e)
+        return []
+
+def get_all_extensions(filenames: list) -> list:
+        extensions = []
+        for filename in filenames:
+            extension = filename.split('.')[-1]
+            if extension not in extensions:
+                extensions.append(extension)
+        return extensions
