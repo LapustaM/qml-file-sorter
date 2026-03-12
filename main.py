000a8fd24478258e797878c91d97520b5a7644dc
mlapusta <lapusta.mariia@gmail.com>
2026-03-12 16:04:19 +0200

    program runs with main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import pathlib
-from utils import create_folder
+from utils import create_folder, get_all_filenames, get_all_extensions, move_file
+import os
 
-path = str(pathlib.Path().resolve())
-create_folder(path, 'images')
+path = str(r"D:\Test_sort")
+
+filenames = get_all_filenames(path)
+print(filenames)
+extensions = get_all_extensions(filenames)
+print(extensions)
+
+for extension in extensions:
+    create_folder(path, extension)
+    for filename in filenames:
+        if filename.endswith(extension):
+            move_file(path, os.path.join(path, extension), filename)
